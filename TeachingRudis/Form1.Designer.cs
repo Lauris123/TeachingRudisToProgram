0868c8122e783a74d98932027960fd1125779d13
Lauris Stanke <laurisstanke123@gmail.com>
2015-10-07 16:52:30 +0300

    added functionality

--- TeachingRudis/Form1.Designer.cs
+++ TeachingRudis/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn1
@@ -48,7 +51,7 @@
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(173, 96);
+            this.lbl1.Location = new System.Drawing.Point(185, 114);
             this.lbl1.Name = "lbl1";
             this.lbl1.Size = new System.Drawing.Size(13, 13);
             this.lbl1.TabIndex = 1;
@@ -62,6 +65,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Buy blender (1)";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -71,6 +75,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Buy squeezer (10)";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -81,11 +86,41 @@
             this.button3.Text = "Buy slave (50)";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(365, 251);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -105,6 +140,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TeachingRudis/Form1.cs
+++ TeachingRudis/Form1.cs
@@ -15,13 +15,55 @@ namespace TeachingRudis
         public Form1()
         {
             InitializeComponent();
+            workingBlender();
+            refresh();
         }
         int x = 0;
+        int blenderCount = 0;
+        int squeezerCount = 0;
         private void btn1_Click(object sender, EventArgs e)
         {
-            x ++;
-            lbl1.Text = x .ToString();
+            x++;
+            lbl1.Text = x.ToString();
             //Laibais 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (x > 0)
+            {
+                x--;
+                blenderCount++;
+                label1.Text = blenderCount.ToString();
+            }
+
+        }
+        private async void refresh()
+        {
+            while (true)
+            {
+                lbl1.Text = x.ToString();
+                await Task.Delay(20);
+            }
+        }
+        private async void workingBlender()
+        {
+            while (true)
+            {
+                x = x + blenderCount;
+                x = x + squeezerCount * 5;
+                await Task.Delay(1000);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (x > 9)
+            {
+               x = x-10;
+                squeezerCount++;
+                label2.Text = squeezerCount.ToString();
+            }
         }
     }
 }
